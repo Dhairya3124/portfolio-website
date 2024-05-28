1e3444d1c73e466e3597e366e991a2199412851e
Dhairya Khanna <dk9582671597@gmail.com>
2024-05-28 22:03:19 +0530

    feat:Completed Skills Section

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import Title from './Title';
-import { SiNextdotjs, SiReact } from 'react-icons/si';
+import { SiGit, SiJavascript, SiMongodb, SiNextdotjs, SiNodedotjs, SiPostgresql, SiReact, SiTailwindcss, SiTypescript } from 'react-icons/si';
 import { HoverEffect } from '@/components/ui/card-hover-effect';
 
 export default function Skills() {
@@ -13,10 +13,38 @@ export default function Skills() {
     {
       text: 'Next.js',
       Icon: SiNextdotjs
-    }
+    },
+    {
+      text: 'Node.js',
+      Icon: SiNodedotjs
+    },
+    {
+      text: 'PostgreSQL',
+      Icon: SiPostgresql
+    },
+    {
+      text: 'Typescript',
+      Icon: SiTypescript
+    },
+    {
+      text: 'MongoDB',
+      Icon: SiMongodb
+    },
+    {
+      text: 'Git',
+      Icon: SiGit
+    },
+    {
+      text: 'Javascript',
+      Icon: SiJavascript
+    },
+    {
+      text: 'Tailwind',
+      Icon: SiTailwindcss
+    },
   ];
   return (
-    <div>
+    <div className='max-w-5xl mx-auto px-8'>
       <Title
         text="Skills 🔪"
         className="flex flex-col items-center justify-center -rotate-6"
