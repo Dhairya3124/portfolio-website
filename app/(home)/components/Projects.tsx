594eb867e1d778550efbc2d1c77ae6647d4eb3b7
Dhairya Khanna <dk9582671597@gmail.com>
2024-06-02 17:24:07 +0530

    feat: Completed Projects Section

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { SiFlask, SiPostgresql, SiPython, SiTailwindcss } from 'react-icons/si';
+import {
+  SiCss3,
+  SiFlask,
+  SiHtml5,
+  SiJavascript,
+  SiPostgresql,
+  SiPython,
+  SiTailwindcss
+} from 'react-icons/si';
 import Title from './Title';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { DirectionAwareHover } from '@/components/ui/direction-aware-hover';
 
 export default function Projects() {
   const projects = [
     {
+      title: 'WikiDataComplete(GsoC 2021)',
+      tech: [SiJavascript, SiHtml5, SiCss3],
+      link: 'https://github.com/WikidataComplete/Wikidata-Complete-Gadget',
+      cover: '/project-1.png',
+      background: 'bg-indigo-500'
+    },
+    {
       title: 'Libsys',
       tech: [SiFlask, SiPython, SiPostgresql, SiTailwindcss],
-      link: '',
-      cover: '',
-      background: 'bg-indigo-500'
+      link: 'https://github.com/Dhairya3124/libmanage',
+      cover: '/project-2.png',
+      background: 'bg-green-500'
     }
   ];
   return (
@@ -19,13 +36,29 @@ export default function Projects() {
         text="Projects 🎨"
         className="flex flex-col items-center justify-center rotate-6"
       />
-      <div className='grid grid-cols-1 sm:grid-cols-2 pt-20'>
-        {projects.map((project,index)=>{
-          return <Link href = {project.link} key={index}>
-            
-          </Link>
+      <div className="grid grid-cols-1 sm:grid-cols-2 pt-20 gap-5">
+        {projects.map((project, index) => {
+          return (
+            <Link href={project.link} key={index}>
+              <div className={cn('p-5 rounded-md', project.background)}>
+                <DirectionAwareHover
+                  imageUrl={project.cover}
+                  className="w-full space-y-5
+              cursor-pointer"
+                >
+                  <div className='space-y-5'>
+                  <h1 className='text-2xl'>{project.title}</h1>
+                  <div className='flex items-center gap-5'>
+                    {project.tech.map((Icon, index) => {
+                      return <Icon className="w-8 h-8" key={index} />;
+                    })}
+                  </div>
+                  </div>
+                </DirectionAwareHover>
+              </div>
+            </Link>
+          );
         })}
-
       </div>
     </div>
   );
