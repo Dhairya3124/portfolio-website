2dc12341ec201b6da101b8daeb41ebc992b0e7c7
Dhairya Khanna <dk9582671597@gmail.com>
2024-06-09 01:06:49 +0530

    feat: Added Full Resume Button

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import { MovingBorderBtn } from '@/components/ui/moving-border';
+import Link from 'next/link';
 export default function Footer() {
+  const resumeURL =
+    'https://drive.google.com/file/d/1aBDoecTm8FOAaiZfHYThQ-D10CXYwX1U/view';
   return (
     <div className="border-t mt-10">
-      <Navbar className="flex-col gap-5"/>
+      <div className='flex flex-col items-center p-2'>
+      <Link href={resumeURL}>
+        <MovingBorderBtn borderRadius="0.5rem" className="p-3 font-semibold">
+          <p>Full Resume🚀</p>
+        </MovingBorderBtn>
+      </Link>
+      </div>
+      <Navbar className="flex-col gap-5" />
     </div>
-    
-  )
+  );
 }
