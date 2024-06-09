66c54ca0769cf0f2a397dc80eb68ec1beb0b6205
Dhairya Khanna <dk9582671597@gmail.com>
2024-06-09 14:30:39 +0530

    fix: Page Width

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,7 +8,7 @@ import Experience from './components/Experience';
 
 export default function page() {
   return (
-    <div className="min-h-[400vh] bg-black overflow-hidden">
+    <div className="min-h-screen bg-black overflow-hidden">
       <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
         <div className="max-w-7xl mx-auto p-5">
           <Navbar />
