128fb84efee9f5352d9d35a2e328cfa481f999b8
Dhairya Khanna <dk9582671597@gmail.com>
2024-06-11 22:09:47 +0530

    fix: Contact Me Button

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function page() {
   return (
     <div className="min-h-screen bg-black overflow-hidden">
       <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
-        <div className="max-w-7xl mx-auto p-5">
+        <div className="max-w-7xl mx-auto p-10">
           <Navbar />
           <HeroSection />
         </div>
